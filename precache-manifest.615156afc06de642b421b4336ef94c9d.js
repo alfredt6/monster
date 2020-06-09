02c4b70f4d52465a3498bfd5580fc198b92bb393
alfredt6 <alfred.salvacion@gmail.com>
2020-06-09 16:51:40 +0800

    Updates

---
+++ precache-manifest.615156afc06de642b421b4336ef94c9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395477075d9a9e67bd5f507ccfaef451",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "32743743824f243e7813",
+    "url": "/monster/static/css/main.868d6c4a.chunk.css"
+  },
+  {
+    "revision": "2c643acc1b1026722189",
+    "url": "/monster/static/js/2.f9462cd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.f9462cd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32743743824f243e7813",
+    "url": "/monster/static/js/main.6f51412c.chunk.js"
+  },
+  {
+    "revision": "29dbeaf9e3f5bfcbde75",
+    "url": "/monster/static/js/runtime-main.279536a6.js"
+  }
+]);
